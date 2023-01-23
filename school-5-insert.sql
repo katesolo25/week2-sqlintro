f6731b6cc1da19d1806b22337fe81580ce3df99e
katesolo25 <ekim25@icloud.com>
2023-01-23 20:29:38 +0000

    creating and editing data tables in SQL

---
+++ school-5-insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO students (
+    'first_name',
+    'last_name',
+    'email'
+) VALUES (
+    'Ben',
+    'Block',
+    'email@nw.edu'
+), (
+    'Kate',
+    'Kim',
+    '123@mail.edu'
+);
---
+++ school-8-insert-again.sql
@@ -0,0 +1,9 @@
+INSERT INTO students (
+    'first_name',
+    'last_name',
+    'email'
+) VALUES (
+    'max',
+    'solo',
+    '129@mail.com'
+);
